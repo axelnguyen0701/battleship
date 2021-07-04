84a662a6f81d7940199c22ffc457c96d9c629a85
Hieu Nguyen <axelnguyen0701@gmail.com>
2021-07-04 16:21:34 -0600

    Hit accept array of arrays

--- src/logic/factory/ship.js
+++ src/logic/factory/ship.js
@@ -1,12 +1,38 @@
-const ShipFactory = (coordinates) => {
-	let hitArray = []
-	const getLength = () => {return coordinates.length}
-	const hit = (coordinate) => {hitArray.push(coordinate)}
-	const isSunk = () => {
-		return coordinates.every(coordinate => hitArray.indexOf(coordinate) !== -1)
-	} 
+import { indexOfForArrays } from "./utilities";
 
-	return {getLength, hit, isSunk}
-}
+const ShipFactory = (coordinates) => {
+  let shipCoordinates = coordinates;
+  let hitArray = [];
+  const getLength = () => {
+    return coordinates.length;
+  };
+  const hit = (attackCoordinate) => {
+    hitArray.push(attackCoordinate);
+  };
+  const isSunk = () => {
+    return shipCoordinates.every(
+      (curCoordinate) => indexOfForArrays(hitArray, curCoordinate) !== -1
+    );
+  };
+
+  const shipName = () => {
+    let name = "";
+    switch (coordinates.length) {
+      case 3:
+        name = "Cruiser";
+        break;
+      case 4:
+        name = "Battleship";
+        break;
+      case 5:
+        name = "Aircraft Carrier";
+        break;
+      default:
+        name = "Destroyer";
+    }
+    return name;
+  };
+  return { getLength, hit, isSunk, shipName, shipCoordinates };
+};
 
 export default ShipFactory;
--- src/logic/test/ship.test.js
+++ src/logic/test/ship.test.js
@@ -1,42 +1,74 @@
-import ShipFactory from '../factory/ship'
+import ShipFactory from "../factory/Ship";
 
 let aircraft_carrier;
 let battleship;
 
 beforeEach(() => {
-	aircraft_carrier = ShipFactory([0,1,2,3,4])
-	battleship = ShipFactory([5,6,7,8])
+  aircraft_carrier = ShipFactory([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ]);
+  battleship = ShipFactory([
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+  ]);
+});
 
-})
+it("accept array of coordinates and have length", () => {
+  expect(aircraft_carrier.getLength()).toBe(5);
+  expect(battleship.getLength()).toBe(4);
+});
 
-it('accept array of coordinates and have length', () => {
-	expect(aircraft_carrier.getLength()).toBe(5);
-	expect(battleship.getLength()).toBe(4);
-})
+it("records hit", () => {
+  aircraft_carrier.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  aircraft_carrier.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  aircraft_carrier.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
 
-it('records hit',() => {
-	aircraft_carrier.hit(5)
-	aircraft_carrier.hit(6)
-	aircraft_carrier.hit(7)
-	
-	battleship.hit(5)
-	battleship.hit(6)
-	battleship.hit(7)
-	
-})	
+  battleship.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  battleship.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  battleship.hit([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+});
 
+it("is able to be sunk", () => {
+  aircraft_carrier.hit([0, 0]);
+  aircraft_carrier.hit([0, 1]);
+  aircraft_carrier.hit([0, 2]);
+  aircraft_carrier.hit([0, 3]);
+  aircraft_carrier.hit([0, 4]);
+  expect(aircraft_carrier.isSunk()).toBe(true);
+  expect(battleship.isSunk()).toBe(false);
+});
 
-it('is able to be sunk', () => {
-	aircraft_carrier.hit(0)
-	aircraft_carrier.hit(1)
-	aircraft_carrier.hit(2)
-	aircraft_carrier.hit(3)
-	aircraft_carrier.hit(4)
-	expect(aircraft_carrier.isSunk()).toBe(true);
-	battleship.hit(0)
-	battleship.hit(1)
-	battleship.hit(2)
-	battleship.hit(4)
-	battleship.hit(3)
-	expect(battleship.isSunk()).toBe(false);
-})
+it("has a name", () => {
+  expect(aircraft_carrier.shipName()).toBe("Aircraft Carrier");
+  expect(battleship.shipName()).toBe("Battleship");
+});
